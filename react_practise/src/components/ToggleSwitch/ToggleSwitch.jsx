08adfb67f45c51cb59f19e0fbe6dd3c8073e1f6f
Anupriya Kumari <anuyadav20001@gmail.com>
2025-04-07 10:37:16 +0530

    toggle

--- react_practise/src/components/ToggleSwitch/ToggleSwitch.jsx
+++ react_practise/src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../ToggleSwitch/ToggleSwitch.css";
 
 const ToggleSwitch = () => {
+  const [IsOn, setIsOn] = useState(false);
+
+  const handleToggleSwitch = () => {
+    setIsOn(!IsOn);
+  };
+
   return (
-    <div>
-      <button>toggle</button>
+    <div
+      className="toggle-switch"
+      onClick={handleToggleSwitch}
+      style={{ backgroundColor: IsOn ? "green" : "" }}
+    >
+      <div className={`switch ${IsOn ? "ON" : "OFF"}`}>
+        <span className="switch-state">{IsOn ? "ON" : "OFF"}</span>
+      </div>
     </div>
   );
 };
-
 export default ToggleSwitch;
