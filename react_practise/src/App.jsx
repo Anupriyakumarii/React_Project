f30a8e71f47c0ce468e7815b0d3e00c55a4ed073
Anupriya Kumari <anuyadav20001@gmail.com>
2025-04-09 10:10:45 +0530

    context

--- react_practise/src/App.jsx
+++ react_practise/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 // import ToggleSwitch from "./components/ToggleSwitch/ToggleSwitch";
-import HookState from "./components/hooks/HookState";
+// import HookState from "./components/hooks/HookState";
 
 function App() {
   return (
     <>
-      <HookState></HookState>
+      {/* <HookState></HookState> */}
       {/* <ToggleSwitch></ToggleSwitch> */}
     </>
   );
--- react_practise/src/components/hooks/ContectApi/ContectApiDemo.jsx
+++ react_practise/src/components/hooks/ContectApi/ContectApiDemo.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const ContectApiDemo = () => {
+export const ContectApiDemo = () => {
   return <div></div>;
 };
-
-export default ContectApiDemo;
